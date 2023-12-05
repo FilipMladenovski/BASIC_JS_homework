0d3e8e2be0cb1329eab0044756b5a73dad1f7ba8
FilipMladenovski <filip_chelsea@live.com>
2023-12-05 17:08:52 +0100

    Homework1

---
+++ Homework1/script.js
@@ -0,0 +1,7 @@
+alert('Homework 1');
+let phoneNumber= 30;
+let phonePrice= 119.95;
+let taxRate= 5;
+let finalPrice = phonePrice * (1+ taxRate/100) * phoneNumber;
+
+console.log('The final price of phones is:',finalPrice);
